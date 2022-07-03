57e61dc256359cdbf3b4d0df4516619adfe02a0f
Xanonymous <trusaidlin@gmail.com>
2022-07-03 12:41:38 +0800

    refactor: simplify factorial tests

--- src/test/kotlin/math/FactorialKtTest.kt
+++ src/test/kotlin/math/FactorialKtTest.kt
@@ -4,18 +4,20 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.security.InvalidParameterException
 
-internal class FactorialIterativeKtTest {
+internal class FactorialKtTest {
     @Test
     fun `should get a normal factorial result`() {
         val input = 6
         val expectedFactorial = 720
         assert(factorialIterative(input) == expectedFactorial)
+        assert(factorialRecursive(input) == expectedFactorial)
     }
 
     @Test
     fun `should throw exception while input is smaller than zero`() {
         val input = -7
         assertThrows<InvalidParameterException> { factorialIterative(input) }
+        assertThrows<InvalidParameterException> { factorialRecursive(input) }
     }
 
     @Test
@@ -23,5 +25,6 @@ internal class FactorialIterativeKtTest {
         val input = 0
         val expectedFactorial = 1
         assert(factorialIterative(input) == expectedFactorial)
+        assert(factorialRecursive(input) == expectedFactorial)
     }
 }
