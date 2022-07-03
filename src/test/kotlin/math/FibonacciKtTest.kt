3d4085f389d9097005b41102f0f8e7d177037fa7
Xanonymous <trusaidlin@gmail.com>
2022-07-03 12:42:29 +0800

    test: add fib test

---
+++ src/test/kotlin/math/FibonacciKtTest.kt
@@ -0,0 +1,12 @@
+package math
+
+import org.junit.jupiter.api.Test
+
+internal class FibonacciKtTest {
+    @Test
+    fun `should get correct fib result`() {
+        val input = 16
+        val expectedFibResult = 987
+        assert(fibRecursive(input) == expectedFibResult)
+    }
+}
