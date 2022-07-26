c24ea7dc488e02fd887d2532431265f92cc225ec
Xanonymous <trusaidlin@gmail.com>
2022-07-27 00:21:03 +0800

    refactor: rename class

--- src/main/kotlin/leetcode/LeetCode1480.kt
+++ src/main/kotlin/leetcode/LeetCode1480.kt
@@ -4,7 +4,7 @@ package leetcode
  * 1480. Running Sum of 1d Array
  * https://leetcode.com/problems/running-sum-of-1d-array/
  * */
-class Solution {
+class LeetCode1480 {
     fun runningSum(nums: IntArray): IntArray {
         for (i in 1 until nums.size) {
             nums[i] += nums[i - 1]
