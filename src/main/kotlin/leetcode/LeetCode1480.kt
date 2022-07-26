a437fb97296eb00d00e2621debb7dd1e6bb9f370
Xanonymous <trusaidlin@gmail.com>
2022-07-27 00:19:15 +0800

    feat: add leetcode 1480

---
+++ src/main/kotlin/leetcode/LeetCode1480.kt
@@ -0,0 +1,15 @@
+package leetcode
+
+/**
+ * 1480. Running Sum of 1d Array
+ * https://leetcode.com/problems/running-sum-of-1d-array/
+ * */
+class Solution {
+    fun runningSum(nums: IntArray): IntArray {
+        for (i in 1 until nums.size) {
+            nums[i] += nums[i - 1]
+        }
+
+        return nums
+    }
+}
